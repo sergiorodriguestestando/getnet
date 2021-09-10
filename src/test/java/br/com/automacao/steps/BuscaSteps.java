beb703da71c715b93ade6feb88d15eb09b2d06cd
sergiorodriguestestando <sergiorodrigues.ti@gmail.com>
2021-09-10 16:03:21 -0300

    organizando

--- src/test/java/br/com/automacao/steps/BuscaSteps.java
+++ src/test/java/br/com/automacao/steps/BuscaSteps.java
@@ -6,45 +6,20 @@ import cucumber.api.java.pt.Quando;
 
 public class BuscaSteps {
 
-
-BuscaPage cp = new BuscaPage();
-
-	
-	@Quando("^cadatrar um cliente no site$")
-	public void cadatrar_um_cliente_no_site() throws Throwable {
-	
-//		cp.selecionarMenuAjuda();
-//		cp.selecionarCentralDeAjuda();
-//		cp.digitarPalavraChave();
-	//	cp.listarResultadosPesquisa();
-		
-	}
-	
-	
-	@Quando("^acessar o site$")
-	public void acessar_o_site() throws Throwable {
-	 
-	}
+	BuscaPage cp = new BuscaPage();
 
 	@Quando("^realizar uma busca por \"([^\"]*)\"$")
 	public void realizar_uma_busca_por(String assunto) throws Throwable {
-		
+
 		cp.selecionarMenuAjuda();
 		cp.selecionarCentralDeAjuda();
 		cp.digitarPalavraChave(assunto);
-	
+
 	}
 
-
-
-	
 	@Então("^o resultado da consulta será \"([^\"]*)\"$")
 	public void o_resultado_da_consulta_será(String resultado) throws Throwable {
 		cp.listarResultadosPesquisa(resultado);
 	}
-
-
-
-	
 
 }
